317e4c85e508df039a40220ed1587529f9557737
IvanAntonov52 <wmwpro@gmail.com>
2025-01-10 22:05:49 +0300

    last homework

---
+++ src/masks.py
@@ -0,0 +1,20 @@
+def get_mask_card_number(numbers: str) -> str:
+    """Принимает на вход номер карты в виде числа и возвращает номер с зашифрованными символами *"""
+    numbers = str(numbers)
+    new_mask_card = numbers[:4] + " " + numbers[6:8] + "** ****" + numbers[-4:]
+    return new_mask_card
+
+
+def get_mask_account(mask_account: str) -> str:
+    """Принимает на вход номер счет в виде числа и возвращает номер счет с зашиврофарнными символами *"""
+    mask_account = str(mask_account)
+    new_mask_account = "**" + mask_account[-4:]
+    return new_mask_account
+
+
+user_card = "5469 4201 1927 5780"
+user_count = "12345678998765432135"
+masked_info = get_mask_card_number(user_card)
+masked_count_info = get_mask_account(user_count)
+print("Зашифрованный номер карты:", masked_info)
+print("Зашифрованный номер счета:", masked_count_info)
