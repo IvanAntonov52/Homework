cd5329fcdc3df7b58fd6d82cac0f1b8667606d14
IvanAntonov52 <wmwpro@gmail.com>
2025-01-11 16:30:35 +0300

    used all linters

--- src/widget.py
+++ src/widget.py
@@ -1,11 +1,9 @@
-from src.masks import get_mask_account
-from src.masks import get_mask_card_number
-from typing import Union
+from src.masks import get_mask_account, get_mask_card_number
 
 
 # Функция принимает на вход строку формата Visa Platinum 7000792289606361, или Maestro 7000792289606361,
 # или Счет 73654108430135874305
-def mask_account_info(card: str): -> str
+def mask_account_info(card: str) -> str:
     """Определяем счет, номер и тип карты"""
     card_split = card.split()
     if "счет" in card.lower():
@@ -14,9 +12,9 @@ def mask_account_info(card: str): -> str
         return f"{''.join(card_split[:-1])} {get_mask_card_number(card_split[-1])}"
 
 
-#Функция принимает дату и преобразовывает ее
+# Функция принимает дату и преобразовывает ее
 def get_date(date_str: str) -> str:
     """принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407" и возвращает строку с датой в формате
-        "ДД.ММ.ГГГГ"("11.03.2024")."""
+    "ДД.ММ.ГГГГ"("11.03.2024")."""
     date_new = date_str[8:10] + "." + date_str[5:7] + "." + date_str[0:4]
     return date_new
