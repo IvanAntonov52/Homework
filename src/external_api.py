dcfebdca2156d486f82887eee76e931babe24a52
IvanAntonov52 <wmwpro@gmail.com>
2025-03-02 13:20:46 +0300

    writing a function in external_api

---
+++ src/external_api.py
@@ -0,0 +1,36 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+
+def get_exchange_rate(currency):
+    """Получает текущий курс валюты к рублю (RUB) через API."""
+    if not API_KEY:
+        raise ValueError("API_KEY не найден в переменных окружения.")
+
+    url = f"https://api.apilayer.com/exchangerates_data/latest?base={currency}&symbols=RUB"
+    headers = {"apikey": API_KEY}
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Проверяем, что запрос успешен
+        return response.json()["rates"]["RUB"]
+    except (requests.RequestException, KeyError):
+        return None
+
+
+def convert_rub(transaction):
+    """Конвертирует сумму транзакции в рубли (RUB)"""
+    amount = transaction["amount"]
+    currency = transaction["currency"]
+
+    if currency == "RUB":
+        return float(amount)
+    elif currency in ["USD", "EUR"]:
+        rate = get_exchange_rate(currency)
+        if rate:
+            return float(amount) * rate
+    return 0.0
