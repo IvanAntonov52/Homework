36e80ce83c892cfed40ecaa350dafd77c0075a58
IvanAntonov52 <wmwpro@gmail.com>
2025-03-26 21:29:26 +0300

    add logging_config.py, and writing logs mask

---
+++ src/logging_config.py
@@ -0,0 +1,16 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+LOG_DIR = BASE_DIR / 'logs'
+LOG_DIR.mkdir(exist_ok=True)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    encoding='utf-8',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    filename=LOG_DIR / 'application.log',
+    filemode='w'
+)
--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,19 @@
+import logging
+
+
+masks_logger = logging.getLogger(__name__)
+
+
 def get_mask_card_number(numbers: str) -> str:
     """ Принимает на вход номер карты в виде числа и возвращает номер с зашифрованными символами *"""
+    masks_logger.debug(f"Начало выполнения функции get_mask_card_number с номером: {numbers}")
     numbers = str(numbers)
     if len(numbers) == 16:
         new_mask_card = numbers[:4] + " " + numbers[4:6] + "** **** " + numbers[-4:]
+        masks_logger.debug(f"Маскированный номер карты: {new_mask_card}")
     else:
-        raise ValueError('Ошибка данных')
+        error_msg = "Неверный ввод карты. Номер карты состоит из 16 чисел"
+        raise ValueError(error_msg)
     return new_mask_card
 
 
